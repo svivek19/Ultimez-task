6a23dd226012b389880d14f3c2efbf60a4bdb8b8
svivek19 <svivek030503@gmail.com>
2024-02-07 19:26:15 +0530

    Ultimez technology task finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [input, setInput] = useState('')
+  const [str, setStr] = useState([{ id: 0, title: '' }])
+  const [num, setNum] = useState([{ id: 0, title: '' }])
+
+  const handleBtn = (e) => {
+    e.preventDefault();
+
+    const logic = { id: input.length + 1, title: input }
+
+    isNaN(input) ? setStr([...str, logic]) : setNum([...num, logic])
+
+    setInput('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input
+          placeholder='enter string or number..'
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <button onClick={handleBtn}>Filter</button>
+      </form>
+      <div>
+        <h2>String</h2>
+        {str.map((e, i) => (
+          <h2 key={i}>{e.title}</h2>
+
+        ))}
+      </div>
+
+      <div>
+        <h2>Number</h2>
+        {num.map((e, i) => (
+          <h2 key={i + 1}>{e.title}</h2>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
